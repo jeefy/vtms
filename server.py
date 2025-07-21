42f976747d9fa5cc7e2014b86e879cdbe0c35ea6
Jeffrey Sica <me@jeefy.dev>
2025-07-20 21:40:54 -0500

    persist data
    Signed-off-by: Jeffrey Sica <me@jeefy.dev>

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import time
+import time, sys, signal
 import paho.mqtt.client as mqtt
 import sqlite3
 from src import config
@@ -12,12 +12,26 @@ def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    client.subscribe("lemons/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload)) 
     cur.execute("INSERT INTO telemetry VALUES(?, ?, ?)", (msg.topic, msg.payload, time.time()))
+
+def graceful_shutdown():
+    print()
+    # the will_set is not sent on graceful shutdown by design
+    # we need to wait until the message has been sent, else it will not appear in the broker
+    mqttc.disconnect()
+    mqttc.loop_stop()
+    con.close()
+    print("Graceful shutdown complete.")
+    sys.exit(0)
+
+# catch ctrl-c
+def signal_handler(signum, frame):
+    graceful_shutdown()
 
 
 con = sqlite3.connect('data/logger.db', check_same_thread=False)
@@ -28,4 +42,8 @@ mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 mqttc.connect(config.mqtt_server, 1883, 60)
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
 mqttc.loop_forever()
